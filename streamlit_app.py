97ffcc7d4789f133ed824377b632da8e94020cb0
SivaSubramanyan <95543928+SivaSubramanyan@users.noreply.github.com>
2022-01-07 14:52:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,53 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+from datetime import date
+from pymongo import MongoClient
 
-"""
-# Welcome to Streamlit!
+client = pymongo.MongoClient("mongodb+srv://Shiva:Namachivaya@cluster0.trwfg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+def update_category(str1):
+        cate.insert_one({'category':str1})
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+def update_sub_category(str1,str2):
+        scate.insert_one({'category': str1,'sub-category':str2})
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+db = client.get_database('study_db')
+cate = db['category']
+scate=db['sub-category']
+description = db['description']
+x=[i['category'] for i in cate.find()]
+y=[i['category'] for i in scate.find()]
+st.title("Hi Karthikeyan :smile: {}".format(str(date.today())))
+selected_date=st.date_input("Enter date")
+categories=st.selectbox("Select a category",x)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+with st.expander("Couldn't found a category? To add one click '+'"):
+    col1, col2 = st.columns([3, 1])
+    with col1:
+        st.write("")
+        add_categoty = st.text_input('Add a category')
+    with col2:
+        st.write("")
+        st.write("")
+        st.write("")
+        if st.button("Add to category","add_category_button","Press to add category"):
+            update_category(add_categoty)
+sub_categories=st.selectbox("Select a sub-category",y)
+with st.expander("Couldn't found a sub-category? To add one click '+'"):
+    sub_col1, sub_col2= st.columns([3,1])
+    with sub_col1:
+        add_sub_categoty = st.text_input('Add a sub-category')
+    with sub_col2:
+        st.write("")
+        st.write("")
+        st.write("")
+        if st.button("Add to sub-category","add_sub-category_button","Press to add sub-category"):
+            update_sub_category(categories,add_sub_categoty)
+txt = st.text_area('Topics Learnt Today')
+percentage_completed=st.number_input("Percentage completed",0,100,0)
+notes = st.text_area('Note')
+reference=st.text_input("Put reference here for future use")
+dictionary={'Date':str(selected_date),"Category":categories,"Sub_Category":sub_categories,"Topics":str(txt.split('\n')),'Percentage_Completed':percentage_completed,"Notes":notes,"Reference":reference}
+if st.button("Add item to cloud"):
+    description.insert_one(dictionary)
+client.close()
